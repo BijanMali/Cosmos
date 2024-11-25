a50a8005115ea9ab3b77e2e683b50c34dc1ccd0a
BijanMali <bijanmali00@gmail.com>
2024-11-25 16:47:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+    const navBtn = document.querySelector('#navBtn');
+    navBtn.classList.toggle('show');
+}
